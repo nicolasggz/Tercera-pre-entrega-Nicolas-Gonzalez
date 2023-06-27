350e120bdb64e24e6180adcf07fda464d8f0b114
nicolasggz <nicolasglgz@gmail.com>
2023-06-26 23:34:36 -0300

    avance de tp3

--- entrega/settings.py
+++ entrega/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'entrega.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- inicio/views.py
+++ inicio/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse('<h1>Ejemplo de Pagina</h1>')
+    return render(request, 'inicio/inicio.html')
